8aed4c19db69f5b5c808c64de64dfba2e965fa62
RuchikaB <rchk.banerjee@gmail.com>
2015-04-28 02:12:03 +0530

    Added LocalDatabase to fetch all local records

---
+++ app/src/main/java/com/iiitd/hostel/Database/LocalDatabase.java
@@ -0,0 +1,38 @@
+package com.iiitd.hostel.Database;
+
+import android.content.Context;
+
+import com.iiitd.hostel.backend.quoteApi.model.Quote;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ruchika on 4/28/15.
+ */
+public class LocalDatabase {
+    Context context;
+
+
+    public List<Quote> getLocalQuotes(Context context) {
+
+        ListOperations lstDBoperation;
+        lstDBoperation = new ListOperations(context);
+        lstDBoperation.open();
+        List<ListDetails> list = lstDBoperation.getAllItems();
+
+        List<Quote> listquote = new ArrayList<Quote>();
+        for(ListDetails ld : list){
+
+            Quote q = new Quote();
+            q.setItemId(ld.getItem_Id());
+            q.setQuantity(ld.getQuantity());
+            listquote.add(q);
+
+        }
+        return listquote;
+
+
+    }
+
+}
