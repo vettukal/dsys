8c59513d972cee94a740afd0550bc24caa104ac4
vettukal <vettukal@gmail.com>
2015-04-26 21:59:43 -0700

    Getting deployed to the server but no new features getting added.

--- app/src/main/java/com/iiitd/hostel/EndpointsAsyncTask.java
+++ app/src/main/java/com/iiitd/hostel/EndpointsAsyncTask.java
@@ -23,8 +23,29 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
     @Override
     protected String doInBackground(Pair<Context, String>... params) {
         if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                    .setRootUrl("https://dsys-project.appspot.com/_ah/api/");
+
+            int test = 0;// 0->checking from the GAE; 1-> local server.
+            MyApi.Builder builder = null;
+            if(test==0){
+                builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("https://dsys-project.appspot.com/_ah/api/");
+            }
+            else{
+                builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+            }
+
+
             // end options for devappserver
 
             myApiService = builder.build();
